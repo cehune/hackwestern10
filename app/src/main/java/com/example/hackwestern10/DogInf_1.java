3e76ed40507db9b9bea0e5cb528031a5d58fa5bd
cecil <ceciliazyy381@gmail.com>
2023-11-25 14:26:48 -0500

    dog info page template

--- app/src/main/java/com/example/hackwestern10/DogInf_1.java
+++ app/src/main/java/com/example/hackwestern10/DogInf_1.java
@@ -2,13 +2,30 @@ package com.example.hackwestern10;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ClipData;
 import android.os.Bundle;
 import android.view.View;
 import android.content.Intent;
 import android.widget.Button;
+import android.widget.Toast;
+import androidx.appcompat.widget.SearchView;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class DogInf_1 extends AppCompatActivity {
     private Button button1;
+    private SearchView search_dog;
+
+
+    private String Doglist[]
+            = { "golden r", "hairy",
+            "Languages", "Interview Corner",
+            "GATE", "ISRO CS",
+            "UGC NET CS", "CS Subjects",
+            "Web Technologies" };
+
+
     @Override
 
 
@@ -17,6 +34,8 @@ public class DogInf_1 extends AppCompatActivity {
         setContentView(R.layout.doginf_1);
 
         button1 = (Button) findViewById(R.id.button);
+
+
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -24,9 +43,45 @@ public class DogInf_1 extends AppCompatActivity {
             }
 
         });
+
+        /*
+        search_dog = findViewById(R.id.search_dog);
+
+        search_dog.clearFocus();
+
+        search_dog.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                filterList(newText);
+                return true;
+            }
+        });
+
     }
 
-        public void open_MainActivity2() {
+    private void filterList(String text) {
+        List<String> filteredList = new ArrayList<>();
+        for(String item : Doglist){
+            if(item.contains(text.toLowerCase())){
+                filteredList.add(item);
+            }
+        }
+
+        if(filteredList.isEmpty() ){
+            Toast.makeText(this, "No Such Dog Brand Found", Toast.LENGTH_SHORT).show();
+        }else{
+            // to do, need item adaptor.
+        }
+*/
+
+    }
+
+    public void open_MainActivity2() {
             Intent intent1 = new Intent(this, DogInf_2.class);
             startActivity(intent1);
 
--- app/src/main/java/com/example/hackwestern10/DogInf_2.java
+++ app/src/main/java/com/example/hackwestern10/DogInf_2.java
@@ -1,14 +1,30 @@
 package com.example.hackwestern10;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class DogInf_2 extends AppCompatActivity {
 
+    Button back_button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.doginf_2);
+
+        back_button = findViewById(R.id.back_button);
+        back_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
+
+
 }
